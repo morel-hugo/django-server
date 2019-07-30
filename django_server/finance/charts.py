0f0efbb4329b2604003d0f03f5098d8ebf3c3bfa
Hugo Morel <hugomorel@MacBook-Pro-de-Hugo.local>
2019-07-30 11:30:52 +0200

    add django_server

--- django_server/finance/charts.py
+++ django_server/finance/charts.py
@@ -1,31 +1,94 @@
 import pygal
 from pygal.style import Style
 
-custom_style = Style(
+custom_style_line = Style(
     background='transparent',
-    transition='400ms ease-in'
+    foreground='#000000', # Legend x,y
+    foreground_strong='#53A0E8', # Strong value and hover value
+    opacity_hover='.5',
+
+    colors=('#e5849b', '#e5849b', '#f3d387', '#8fd9b3', '#E89B53'),
+    transition='400ms ease-in',
 )
-  #value_font_size=100,
-  #no_data_font_size='100')
+
+custom_style_pie = Style(
+    background='transparent',
+    #plot_background='transparent',
+    transition='400ms ease-in',
+
+)
 
 custom_style_gaug = Style(
     background='transparent',
     plot_background='transparent',
     print_values=False,
-    value_font_size=130,
+    value_font_size=140,
+    transition='400ms ease-in',
+    colors=('#90a9e7','#90a9e7'),
 )
 
 
+# ------------   Basic_Line   ------------
 def basic_line():
-    line_chart = pygal.Line(show_legend=False,style=custom_style)
+    line_chart = pygal.StackedLine(fill=True,show_legend=False, style=custom_style_line,height=400, explicit_size=True,show_y_guides=False)
     line_chart.x_labels = map(str, range(2002, 2013))
-    line_chart.add('IE',  [85.8, 84.6, 84.7, 74.5,   66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
+    line_chart.add('IE',  [10.8, 15.6, 37.7, 25.5,   50, 52.6, 54.7, 44.8, 56.2, 60.6, 50.1])
 
     return line_chart.render_data_uri()
 
+# ------------   Selid_pie   ------------
 
-def solid_gauge():
+def solid_pie():
+    pie_chart = pygal.Pie(inner_radius=.75,height=700,style=custom_style_pie,show_legend=False)
+    pie_chart.add('Firefox', 36.6)
+    pie_chart.add('Chrome', 36.3)
+    pie_chart.add('Safari', 4.5)
+    pie_chart.add('Opera', 2.3)
+
+    return pie_chart.render_data_uri()
+
+
+# ------------   Solid_Gauge   ------------
+
+def solid_gauge(data):
     gauge = pygal.SolidGauge(inner_radius=0.85, style=custom_style_gaug, show_legend=False, js=[])
-    gauge.add('Series 6', 77)
+    gauge.add('Series 6', data)
+    return gauge.render_data_uri()
+
+
+def func_solid_gauge():
+    data = 40
+    list_gauge = list()
+    for i in range(4):
+        list_gauge.append(solid_gauge(data))
+
+    return list_gauge
+
+# -----------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+def solid_gauge_fd():
+    gauge = pygal.SolidGauge(inner_radius=0.85, style=custom_style_gaug, show_legend=False, js=[])
+    gauge.add('Series 6', 27)
+
+    return gauge.render_data_uri()
+
+def solid_gauge_marge():
+    gauge = pygal.SolidGauge(inner_radius=0.85, style=custom_style_gaug, show_legend=False, js=[])
+    gauge.add('Series 6', 57)
+
+    return gauge.render_data_uri()
+
+def solid_gauge_nt():
+    gauge = pygal.SolidGauge(inner_radius=0.85, style=custom_style_gaug, show_legend=False, js=[])
+    gauge.add('Series 6', 87)
 
     return gauge.render_data_uri()
--- django_server/finance/views.py
+++ django_server/finance/views.py
@@ -7,6 +7,13 @@ from . import charts
     #info_account = {"balance":1, "fd":2, "marge":3,"nt":4}
     #return render(request,'pages/index.html', {'info_account': info_account,'chart' : charts.info_gauge})
 
+info_account = {"balance":1, "fd":2, "marge":3,"nt":4}
+
+
+
+list_index = {'info_account': info_account,'chart' : charts.basic_line(),'gauge_0' : charts.func_solid_gauge()[0],\
+'gauge_1' : charts.func_solid_gauge()[1],'gauge_2' : charts.func_solid_gauge()[2],'gauge_3' : charts.func_solid_gauge()[3],\
+'chart3' : charts.solid_pie()}
+
 def index(request):
-    info_account = {"balance":1, "fd":2, "marge":3,"nt":4}
-    return render(request,'pages/index.html', {'info_account': info_account,'chart' : charts.basic_line(),'chart2' : charts.solid_gauge()})
+    return render(request,'pages/index.html', list_index)
